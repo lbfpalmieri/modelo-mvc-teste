8e3d4ac797a113c6a056730fb66458535c29ad77
Lucas Palmieri <lucas.palmieri@outlook.com>
2019-06-26 12:54:57 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salls Web MVC App from C# Course";
+            ViewData["Developer"] = "Lucas Palmieri";
 
             return View();
         }
